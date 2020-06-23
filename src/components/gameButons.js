8b5eb983916d0dbe1b230e6340bf5f0db63c7253
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-06-22 21:21:44 -0400

    setup game buttons - random, clear, next gen, stop, play

--- src/components/gameButons.js
+++ src/components/gameButons.js
@@ -2,4 +2,15 @@ import React from 'react';
 
 const GameButtons = (props) => (
   <div className='game-buttons'>
-   
+    <button onClick={props.randomMix} disabled={props.running}
+    />
+    <button onClick={props.clear} />
+    <button onClick={props.next} />
+    {props.playing ?
+      <button onClick={props.stop} /> :
+      <button  onClick={props.play} />
+    }
+  </div>
+);
+
+export default GameButtons;
